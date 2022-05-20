6661a725ddf83bb55d23668d83bfaa0049d818f9
Oluwatoyin John Adebusola <101136827+johbadebusola@users.noreply.github.com>
2022-05-21 02:20:24 +0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,29 @@
+document.querySelector("#banner").setAttribute("style", "color:white; display:flex; justify-content:space-around; font-family: 'League Spartan', sans-serif; ");
+
+document.querySelector("#logo").setAttribute("style", "font-size:2em; margin-left:-2em;");
+
+document.querySelector("#logo2").setAttribute("style", "margin-top:3em; ");
+
+document.querySelector("#calc-grid").setAttribute("style", " margin:0.5em; font-family: 'League Spartan', sans-serif; " );
+
+document.querySelector("#output").setAttribute("style", "width:100%; min-height:12vh; background-color:hsl(224, 36%, 20%); border-radius:10px; display:flex; justify-content:flex-end; align-items:center; font-size:2em; color:white; ");
+
+document.querySelector("#output-txt").setAttribute("style","  margin-right:0.5em; margin-left:0.5em; display:flex; word-break:break-word;");
+
+document.querySelector("#buttons").setAttribute(
+"style", "  border-radius:10px; background-color:hsl(223, 31%, 20%);margin-top:2em; display:grid; grid-template-columns: repeat(3, 1fr);  grid-template-rows: repeat(6, 1fr); padding:1.5em; grid-gap:1em; min-height:50vh;" 
+)
+
+  //document.querySelector("#buttons").setAttribute("style", "  border-radius:10px; background-color:hsl(223, 31%, 20%);margin-top:2em; display:grid; grid-template-columns: repeat(3, 1fr);grid-template-rows: repeat(6, 1fr); padding:1.5em; grid-gap:1em; min-height:80vh;");
+
+
+function output(){
+    console.log(this.value);
+ document.getElementById("output-txt").innerHTML += this.value;
+}
+
+let num =
+document.querySelectorAll(".num-btn");
+num.forEach(e => {
+    e.addEventListener("click", output)
+})
